356680d817774500eb90491fa845377f748f4806
Antonio-Tone <antoniotone815@gmail.com>
2023-08-22 13:49:40 +0200

    update

--- api/index.js
+++ api/index.js
@@ -2,6 +2,15 @@ const { express, routes } = require("./controller");
 const path = require("path");
 const port = +process.env.PORT || 3000;
 const app = express();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Request-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Expose-Headers", "Authorization");
+  next();
+})
 
 app.use(express.static("./static"));
 
